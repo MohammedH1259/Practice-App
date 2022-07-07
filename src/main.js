1807a6e9041e0d627bb7146c846a18b88a9a7f19
MohammedH1259 <mohammed.hussain12599@gmail.com>
2022-07-07 12:17:47 -0400

    2 files modified, 1 added

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 const { createApp } = require('vue');
-import App from "./App.vue";
+import App from './App.vue';
+import store from './store';
 
-createApp(App).mount("#app");
+
+
+
+const app = createApp(App);
+app.use(store);
+app.mount('#app');
